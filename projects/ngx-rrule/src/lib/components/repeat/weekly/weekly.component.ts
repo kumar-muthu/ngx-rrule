d739e5fd1762bc9cbbf92762a82a4102f256f1f7
Muthukumar Rajendran <kumar_muthu@hotmail.com>
2019-04-24 09:27:15 +0530

    Weekly Component

--- projects/ngx-rrule/src/lib/components/repeat/weekly/weekly.component.ts
+++ projects/ngx-rrule/src/lib/components/repeat/weekly/weekly.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, forwardRef, EventEmitter, Output} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'ngx-weekly',
   templateUrl: './weekly.component.html',
-  styleUrls: ['./weekly.component.css']
+  styleUrls: ['./weekly.component.css'],
+  providers: [{provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => WeeklyComponent), multi: true}]
 })
-export class WeeklyComponent implements OnInit {
+export class WeeklyComponent implements OnInit, ControlValueAccessor {
+  public weeklyForm: FormGroup;
+  @Output() onChange = new EventEmitter();
 
-  constructor() { }
+  public days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.weeklyForm = this.formBuilder.group({
+      mon: false,
+      tue: false,
+      wed: false,
+      thu: false,
+      fri: false,
+      sat: false,
+      sun: false,
+      weeklyInterval: 0,
+    });
   }
 
+  writeValue = (input: any): void => {
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  propagateChange = (_: any) => {
+  }
+
+  onFormchange = () => {
+    this.propagateChange('');
+    this.onChange.emit('');
+  }
 }
--- projects/ngx-rrule/src/lib/ngx-rrule.module.ts
+++ projects/ngx-rrule/src/lib/ngx-rrule.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { NgxRruleComponent } from './ngx-rrule.component';
 import { StartComponent } from './components/start/start.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EndComponent } from './components/end/end.component';
 import { RepeatComponent } from './components/repeat/repeat.component';
 import { HourlyComponent } from './components/repeat/hourly/hourly.component';
@@ -10,11 +10,14 @@ import { DailyComponent } from './components/repeat/daily/daily.component';
 import { WeeklyComponent } from './components/repeat/weekly/weekly.component';
 import { MonthlyComponent } from './components/repeat/monthly/monthly.component';
 import { YearlyComponent } from './components/repeat/yearly/yearly.component';
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [NgxRruleComponent, StartComponent, EndComponent, RepeatComponent, HourlyComponent, DailyComponent, WeeklyComponent, MonthlyComponent, YearlyComponent],
   imports: [
+    BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule
   ],
   exports: [NgxRruleComponent]
