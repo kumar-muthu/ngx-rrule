4e9d403d25e5b5971ec8d0aff49295405363df48
Zain Ahmad Khan <niazi_zain@hotmail.com>
2020-07-10 15:37:55 +0500

    Fixing BrowserModule issue
    Switching BroswerModule to CommonModule to solve the LazyLoad Module import problem.

--- projects/ngx-rrule/src/lib/ngx-rrule.module.ts
+++ projects/ngx-rrule/src/lib/ngx-rrule.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxRruleComponent } from './ngx-rrule.component';
 import { StartComponent } from './components/start/start.component';
-import {NgbDateAdapter, NgbDateNativeAdapter, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateNativeAdapter, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EndComponent } from './components/end/end.component';
 import { RepeatComponent } from './components/repeat/repeat.component';
 import { WeeklyComponent } from './components/repeat/weekly/weekly.component';
 import { MonthlyComponent } from './components/repeat/monthly/monthly.component';
 import { YearlyComponent } from './components/repeat/yearly/yearly.component';
-import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [NgxRruleComponent, StartComponent, EndComponent, RepeatComponent, WeeklyComponent, MonthlyComponent, YearlyComponent],
   imports: [
-    BrowserModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule
   ],
   providers: [
-    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter},
+    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter },
   ],
   exports: [NgxRruleComponent]
 })
